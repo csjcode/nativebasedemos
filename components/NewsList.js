e3a3de1d4eb83b55dda7e170e654c0c7cd4cf4df
Chris <christophersstjohn@gmail.com>
2017-03-04 14:53:41 +0700

    News List with onsite mock data

---
+++ components/NewsList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Container, Content, Icon, Body, List, ListItem } from 'native-base';
+
+const items = ['News', 'user', 'Tech', 'user', 'News', 'user','New York City', 'user']
+
+
+export const dataMessages = [
+  {'category': 'News', 'user':'nbc', 'message': 'France looks at election results','rating':'26'},
+  {'category': 'Tech', 'user': 'google', 'message': 'Google looking to buy a robot company','rating':'5'},
+  {'category': 'News', 'user': 'cbs', 'message': 'More Fighting in Iraq','rating':'12'},
+  {'category': 'Finance', 'user': 'wallstreetjournal', 'message': 'Stock Market Reaches New Highs','rating':'15'},
+  {'category': 'New York City', 'user': 'bobdobbs', 'message': 'SoHo getting makeover, see Houston','rating':'22'},
+  {'category': 'Personal', 'user': 'mike', 'message': 'Hey whats up man haven \'t heard from you in a while','rating':'56'}
+];
+
+class NewsList extends Component {
+
+  render() {
+    return (
+        <Container>
+            <Content>
+                <List dataArray={dataMessages} renderRow={(data) =>
+                    <ListItem>
+                        <Text>{data.category} - {data.user} - {data.message} - {data.rating}</Text>
+                    </ListItem>
+                } />
+            </Content>
+        </Container>
+    );
+  }
+
+}
+
+export default NewsList;
